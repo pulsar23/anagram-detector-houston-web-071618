ca1bdf3c46c8564f57352e1f5019a14914e893d2
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-22 19:58:19 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end #initialize
+
+  def match(array)
+    found = []
+    word_to_match = @word.split(//)
+    word_to_match = word_to_match.sort!
+    array.each do |word2|
+       word2_compare = word2.split(//)
+       word2_compare.sort!
+       if word_to_match == word2_compare
+         found << word2
+       end #if
+    end #array.each
+    found
+  end #match
+
+end #class Anagram
